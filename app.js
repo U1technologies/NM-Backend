a14c9b48a23de1b498c638fcc85eda57ed79d992
Garima1805 <garima.singh@moondive.co>
2026-03-08 00:54:53 +0530

    fix: update CORS to allow production frontend URL

--- app.js
+++ app.js
@@ -37,7 +37,10 @@ const app = express();
 
 // Middlewares
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: [process.env.FRONTEND_URL, 'http://localhost:3000'],
+  credentials: true
+}));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
